00ba5da789edf4c3782f0c254c945613b8df469f
abel <abelaatkelet@gmail.com>
2025-02-08 20:15:54 +0300

    styling

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div className="root-container">
+    <div className="root-container py-5 px-5">
      <Button>Click me</Button>
     </div>
   );
--- components/header.tsx
+++ components/header.tsx
@@ -1,11 +1,30 @@
-import React from 'react'
+import Link from "next/link";
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
-const header = () => {
+const Header = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <header className="my-10 flex justify-between gap-5">
+      <Link href="/">
+        <Image src="/icons/logo.svg" alt="logo" width={40} height={40} />
+      </Link>
 
-export default header
+      <ul className="flex flex-row items-center gap-8">
+        <li>
+          <form
+            action={async () => {
+              "use server";
+
+              await signOut();
+            }}
+            className="mb-10"
+          >
+            <Button>Logout</Button>
+          </form>
+        </li>
+      </ul>
+    </header>
+  );
+};
+
+export default Header;
